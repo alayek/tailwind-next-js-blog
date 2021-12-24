ee5da63b8ce6c8c8289bbb765e66fb382695788b
alayek <5607371+alayek@users.noreply.github.com>
2021-12-24 12:32:16 +0530

    temp: update gh-pages

---
+++ _next/static/hHnZgYR1rVnd0eHbAG3Qi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-76fc561ac76f198b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
